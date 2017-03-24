071d87bac4329c035ae320dc0f31bc414a03c026
Neilson <hc3wong@uwaterloo.ca>
2017-03-24 16:49:28 -0400

    tuple

--- MediaDedup.py
+++ MediaDedup.py
@@ -1,9 +1,11 @@
-#load show titles db
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import csv
 
 shows = {}
 locations = {}
+folders = []
 
 #load titles into our db
 with open('anime-titles.dat.gz.txt') as csvFile:
@@ -37,9 +39,38 @@ def filterSubMeta(filename):
 def getTitle(stripped):
 	return stripped.rstrip("- 1234567890")
 def getEp(stripped):
-	return stripped[stripped.rfind("-")+1:].strip();
+	return stripped[stripped.rfind("-")+1:].strip()
+def getAId(title):
+	return shows.get(title)
 
-stripper = filterSubMeta("[HorribleSubs] Masamune-kun no Revenge - 12 [720p].mkv")
-tit = getTitle(stripper)
-num = getEp(stripper)
-print(tit + " ep:" + num)
+def getData(filename):
+	stripped = filterSubMeta(filename)
+	title = getTitle(stripped)
+	num = getEp(stripped)
+	aid = getAId(title)
+	return (aid, title, num)
+
+def scanFolders():
+	#scan each of the folders, and catalogue the locations for each of the files
+	
+	return
+
+
+
+
+
+
+
+def test():
+	#"[HorribleSubs] Masamune-kun no Revenge - 12 [720p].mkv"
+	data = getData("[HorribleSubs] Masamune-kun no Revenge - 12 [720p].mkv");
+	print("Aid: " + data[0])
+	print("Title: " + data[1]);
+	print("Ep: " + data[2])
+
+	data2 = getData("[HorribleSubs] 政宗くんのリベンジ - 6 [1080p].mkv");
+	print("Aid: " + data2[0])
+	print("Title: " + data2[1]);
+	print("Ep: " + data2[2])
+
+test();
